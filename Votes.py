a5c77d1ff646f56786cb4861af78126fb6184574
Pablo Ribeiro, Pablotic0 <125490539+pabloriber@users.noreply.github.com>
2024-09-19 22:02:01 -0300

    Add files via upload

---
+++ Votes.py
@@ -0,0 +1,25 @@
+Michael = 1
+Jena = 3
+Paul = 3
+James = 7
+
+if James < 4 :
+    print('James Lose')
+elif James > 4:
+    print('James Won')
+  
+
+if Michael < 4:
+    print('Michael Lose')
+elif Michael > 4:
+    print('Michael Won')
+
+if Jena  < 4:
+    print('Jena Lose')
+elif Jena > 4:
+    print('Jena Won')
+
+if Paul < 4:
+    print('Paul Lose')
+elif Paul > 4:
+    print('Paul Won')
